d9be9ea5c95636718a6e5e3172b91b3812f6e58f
mas rosyid <rosyidgrobogan@gmail.com>
2020-03-23 21:23:52 +0700

    add UserService.java

---
+++ src/main/java/com/sekolahbackend/entity/CartDetail.java
@@ -0,0 +1,25 @@
+package com.sekolahbackend.entity;
+
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.hibernate.annotations.Where;
+//
+//import javax.persistence.*;
+//
+//@Getter
+//@Setter
+//@Entity
+//@Table(name = "cart_detail")
+//@Where(clause = "status = 'ACTIVE'")
+public class CartDetail extends Persistence {
+
+//    private static final long serialVersionUID = 1130010943968579177L;
+//    @JoinColumn(name = "cart_id")
+//    @ManyToOne(targetEntity = Book.class, fetch = FetchType.LAZY)
+//    private Book book;
+//
+//    @JoinColumn(name = "cart_id")
+//    @ManyToOne(targetEntity = Cart.class, fetch =
+//            FetchType.LAZY)
+//    private Cart cart;
+}
---
+++ src/main/java/com/sekolahbackend/service/UserService.java
@@ -0,0 +1,6 @@
+package com.sekolahbackend.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService  extends UserDetailsService {
+}
