d556ed822bdedba2d6c429923bcd1f2326e9723c
mas rosyid <rosyidgrobogan@gmail.com>
2020-03-23 21:30:02 +0700

    add UserRepository.java

--- src/main/java/com/sekolahbackend/repository/UserRepository.java
+++ src/main/java/com/sekolahbackend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.sekolahbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
