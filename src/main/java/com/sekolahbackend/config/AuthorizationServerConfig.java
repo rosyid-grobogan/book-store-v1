56fa52aceb9d8350a19272c43f285d1122dc1ae5
mas rosyid <rosyidgrobogan@gmail.com>
2020-03-23 21:39:20 +0700

    add ResourceServerConfig.java

---
+++ src/main/java/com/sekolahbackend/config/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+package com.sekolahbackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import
+        org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.
+                AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import com.sekolahbackend.service.UserService;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserService userService;
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+    @Value("${security.oauth2.client.id}")
+    private String clientId;
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+    @Value("${security.oauth2.client.access-token-validity-seconds}")
+    private int accessTokenValiditySeconds;
+    @Value("${security.oauth2.client.refresh-token-validity-seconds}")
+    private int refreshTokenValiditySeconds;
+    @Value("${security.oauth2.client.authorized-grant-types}")
+    private String[] authorizedGrantTypes;
+    @Value("${security.oauth2.client.scope}")
+    private String[] scopes;
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws
+            Exception {
+        clients.inMemory()
+                .withClient(clientId)
+                .secret(passwordEncoder.encode(clientSecret))
+                .accessTokenValiditySeconds(accessTokenValiditySeconds) // 12 hours
+                .refreshTokenValiditySeconds(refreshTokenValiditySeconds) // 30 days
+                .authorizedGrantTypes(authorizedGrantTypes)
+                .scopes(scopes)
+                .resourceIds(resourceId);
+    }
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer
+                                  endpoints) {
+        endpoints
+                .accessTokenConverter(accessTokenConverter())
+                .userDetailsService(userService)
+                .authenticationManager(authenticationManager);
+    }
+    @Bean
+    JwtAccessTokenConverter accessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
+}
