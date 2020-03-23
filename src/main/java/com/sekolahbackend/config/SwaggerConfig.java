c8f562a7426a0609c77b98b73a01a06e59f22759
mas rosyid <rosyidgrobogan@gmail.com>
2020-03-23 21:55:03 +0700

    modified for OAuth

--- src/main/java/com/sekolahbackend/config/SwaggerConfig.java
+++ src/main/java/com/sekolahbackend/config/SwaggerConfig.java
@@ -9,10 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -24,14 +30,37 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 				.groupName("APPS").select()
 				.apis(RequestHandlerSelectors.basePackage("com.sekolahbackend"))
 				.paths(regex("/api/rest.*")).build()
+				.securityContexts(Collections.singletonList(securityContext() ))
+				.securitySchemes(Arrays.asList(securitySchema() ))
 				.apiInfo(metaData());
 	}
 
 	private ApiInfo metaData() {
 		return new ApiInfoBuilder()
-				.title("Online Book Store REST API")
+				.title("Online Book Store REST API by Rosyid")
 				.description("\"Enjoy your REST API\"")
 				.version("1.0").build();
+	}
+
+	private OAuth securitySchema() {
+		List<AuthorizationScope> authorizationScopeList = new ArrayList<>();
+		authorizationScopeList.add(new AuthorizationScope("read", "for read operations"));
+		authorizationScopeList.add(new AuthorizationScope("write", "for write operations"));
+		List<GrantType> grantTypes = new ArrayList<>();
+		GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant("/oauth/token");
+		grantTypes.add(passwordCredentialsGrant);
+		return new OAuth("oauth2", authorizationScopeList, grantTypes);
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		final AuthorizationScope[] authorizationScopes = new AuthorizationScope[2];
+		authorizationScopes[0] = new AuthorizationScope("read", "for read operations");
+		authorizationScopes[1] = new AuthorizationScope("write", "for write operations");
+		return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
 	}
 
 	@Override
