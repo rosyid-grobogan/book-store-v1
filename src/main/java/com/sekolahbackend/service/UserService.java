2d97f68ef41c498b01bb6e559469d15b244b7b8c
mas rosyid <rosyidgrobogan@gmail.com>
2020-04-06 17:01:33 +0700

    modified file

--- src/main/java/com/sekolahbackend/service/UserService.java
+++ src/main/java/com/sekolahbackend/service/UserService.java
@@ -1,6 +1,10 @@
 package com.sekolahbackend.service;
 
+import com.sekolahbackend.model.UserModel;
+import com.sekolahbackend.model.UserRequestModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService  extends UserDetailsService {
+
+    UserModel register(UserRequestModel requestModel);
 }
