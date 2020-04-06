402212f6fdaeb1bac6e0ad0a1cb1b81565a121f6
mas rosyid <rosyidgrobogan@gmail.com>
2020-04-06 16:48:10 +0700

    add file

--- src/main/java/com/sekolahbackend/util/FieldsValueMathcValidator.java
+++ src/main/java/com/sekolahbackend/util/FieldsValueMathcValidator.java
@@ -1,4 +1,27 @@
 package com.sekolahbackend.util;
 
-public class FieldsValueMathcValidator {
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class FieldsValueMathcValidator implements ConstraintValidator<FieldsValueMatch, Object> {
+
+    private String field;
+    private String fieldMatch;
+
+    public void initialize(FieldsValueMatch constraintAnnotation ) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        Object fieldValue = new BeanWrapperImpl(value).getPropertyValue(fieldMatch);
+
+        if (fieldValue != null ){
+            return fieldValue.equals(fieldMatch);
+        }else {
+            return fieldMatch == null;
+        }
+    }
 }
